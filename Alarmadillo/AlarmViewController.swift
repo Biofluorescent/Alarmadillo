9d1677bd4943bd33a06a8a4cc4d716ff990ac34f
tquesenberry <tanner.quesenberry@gmail.com>
2019-03-26 16:37:56 -0700

    Base class files initialized, outlets linked

---
+++ Alarmadillo/AlarmViewController.swift
@@ -0,0 +1,44 @@
+//
+//  AlarmViewController.swift
+//  Alarmadillo
+//
+//  Created by Tanner Quesenberry on 3/26/19.
+//  Copyright © 2019 Tanner Quesenberry. All rights reserved.
+//
+
+import UIKit
+
+class AlarmViewController: UITableViewController {
+
+    @IBOutlet var name: UITextField!
+    @IBOutlet var caption: UITextField!
+    @IBOutlet var datePicker: UIDatePicker!
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var tapToSelectImage: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func datePickerChanged(_ sender: Any) {
+        
+    }
+    
+    @IBAction func imageViewTapped(_ sender: Any) {
+        
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Alarmadillo/GroupViewController.swift
@@ -0,0 +1,33 @@
+//
+//  GroupViewController.swift
+//  Alarmadillo
+//
+//  Created by Tanner Quesenberry on 3/26/19.
+//  Copyright © 2019 Tanner Quesenberry. All rights reserved.
+//
+
+import UIKit
+
+class GroupViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func switchChanged(_ sender: UISwitch) {
+        
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
