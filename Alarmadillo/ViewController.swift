25669bac67fd617fa625e0a3036298e59537b8eb
tquesenberry <tanner.quesenberry@gmail.com>
2019-03-26 15:52:46 -0700

    User Interface Creation

---
+++ Alarmadillo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Alarmadillo
+//
+//  Created by Tanner Quesenberry on 3/26/19.
+//  Copyright © 2019 Tanner Quesenberry. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
